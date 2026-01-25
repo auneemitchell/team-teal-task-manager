afcb3c72e9451de252c957bd665099b96d78a69d
mizotii <mztiiiii0@gmail.com>
2026-01-24 22:05:19 -0800

    add dummy tasks component to make sure endpoint is available and retrieves correct data

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Customers from "./components/Customers.jsx";
+import Tasks from "./components/Tasks.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Customers />
+      <div style={{ display: "grid", gap: 24 }}>
+        <Tasks />
+      </div>
     </>
   );
 }
